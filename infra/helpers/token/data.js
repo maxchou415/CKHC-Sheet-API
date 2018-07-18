c7c3eada463c86dcee3e8e1209f6f69b47cf2362
maxchou415 <yihsunmaxchou@icloud.com>
2018-07-18 18:32:12 +0800

    - Add Helper to Get Token Data

--- infra/helpers/token/data.js
+++ infra/helpers/token/data.js
@@ -1,2 +1,8 @@
-module.exports = async () => {
+const jwt = require('jsonwebtoken')
+
+const secret = process.env.tokenSecret
+
+module.exports = async (incomingToken) => {
+  let data = jwt.verify(incomingToken, secret)
+  return data
 }
