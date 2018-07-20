835ff793e822cd0ffad7b5138c0ee3103e9ab2f6
maxchou415 <yihsunmaxchou@icloud.com>
2018-07-20 19:49:35 +0800

    - Add Token Helper

--- infra/helpers/token/data.js
+++ infra/helpers/token/data.js
@@ -2,7 +2,14 @@ const jwt = require('jsonwebtoken')
 
 const secret = process.env.tokenSecret
 
-module.exports = async (incomingToken) => {
-  let data = jwt.verify(incomingToken, secret)
+module.exports = (incomingToken) => {
+  let data
+  try {
+    data = jwt.verify(incomingToken, secret, {
+      algorithms: ['HS512']
+    })
+  } catch (error) {
+    throw new Error('TOKEN_VERIFY_FAILED')
+  }
   return data
 }
