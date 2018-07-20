e41f2c21df6f7615b56d117959483e243463d17e
maxchou415 <yihsunmaxchou@icloud.com>
2018-07-21 03:41:23 +0800

    Revert "- Add new APIs"
    This reverts commit 99e3889c12bb8f930ef8e1851ef9f3893778ae9e.

--- routes/api/v1/index.js
+++ routes/api/v1/index.js
@@ -1,16 +1,9 @@
 const router = require('koa-router')()
-const { auth } = require('./utils/middlewares')
 
 router.prefix('/api/v1')
 
 // Auth
 router.post('/auth/login', require('./auth').login)
-router.post('/auth/signup', auth.required, auth.role(100), require('./auth').signup)
-
-router.get('/sheets', auth.required, auth.role(1), require('./sheets').get)
-router.get('/sheets/:sheetId', auth.required, auth.role(1), require('./sheets').getById)
-router.post('/sheets', auth.required, auth.role(1), require('./sheets').post)
-router.patch('/sheets/:sheetId', auth.required, auth.role(1), require('./sheets').patchById)
-router.delete('/sheets/:sheetId', auth.required, auth.role(1), require('./sheets').deleteById)
+router.post('/auth/signup', require('./auth').signup)
 
 module.exports = router
