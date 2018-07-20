b39ad5cb3fb261ca50a82a9a6b507b84b3ac1029
maxchou415 <yihsunmaxchou@icloud.com>
2018-07-21 03:41:50 +0800

    - Add Signup API

--- routes/api/v1/auth/signup.js
+++ routes/api/v1/auth/signup.js
@@ -1,3 +1,36 @@
-module.exports = async () => {
+const uniqid = require('uniqid')
+const helper = require('../../../../infra/helpers/index')
+const {
+  user: User
+} = require('../../../../infra/schema/index')
 
+module.exports = async (ctx, next) => {
+  const wantedData = ctx.request.body
+
+  // Make sure incoming datas are following the spec
+  if (typeof wantedData.studentNumber !== 'number') {
+    ctx.throw(400, 'ADD_USER_FAILED')
+  }
+
+  // email and studentNumber are unique
+  const data = {
+    userId: uniqid('user-'),
+    name: String(wantedData.name),
+    email: String(wantedData.email),
+    password: helper.password.hash(String(wantedData.password)),
+    studentNumber: Number(wantedData.studentNumber),
+    createdAt: new Date().toISOString()
+  }
+
+  try {
+    await new User(data).save()
+  } catch (error) {
+    ctx.throw(400, 'ADD_USER_FAILED')
+  }
+
+  ctx.status = 200
+  ctx.body = {
+    result: 'success',
+    data: {}
+  }
 }
